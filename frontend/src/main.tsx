9cafc3156b732c2a7bc60422f640d041a5f76466
SERAVEEM <seraveeem@gmail.com>
2026-06-06 03:01:36 +0700

    add splash screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'lenis/dist/lenis.css'
@@ -28,32 +28,53 @@ import AdminAddProduct from './pages/admin/AdminAddProduct.tsx'
 import AdminEditProduct from './pages/admin/AdminEditProduct.tsx'
 import AdminOrders from './pages/admin/AdminOrders.tsx'
 import AdminOrderDetails from './pages/admin/AdminOrderDetails.tsx'
+import SplashScreen from './components/SplashScreen.tsx'
+
+function RootApp() {
+  const [showSplash, setShowSplash] = useState(() => {
+    // Show splash screen only if it hasn't been shown in the current browser session
+    return sessionStorage.getItem('larasana_splash_shown') !== 'true';
+  });
+
+  const handleSplashComplete = () => {
+    sessionStorage.setItem('larasana_splash_shown', 'true');
+    setShowSplash(false);
+  };
+
+  return (
+    <>
+      {showSplash && <SplashScreen onComplete={handleSplashComplete} />}
+      <BrowserRouter>
+        <SmoothScroll>
+          <Routes>
+            <Route path="/" element={<><Navbar /><LandingPages /><StoryTelling /><HeroShowcasePage /><Footer /></>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/Impact" element={<><Navbar/><ImpactPages /><Footer /></>} />
+            <Route path="/Story" element={<><Navbar /><Story /><Footer /></>} />
+            <Route path="/aboutus" element={<><Navbar/><AboutUs /><Footer /></>} />
+            <Route path="/product/:id" element={<><Navbar /><ProductDetailPage /><Footer /></>} />
+            <Route path="/checkout" element={<><Navbar /><CheckoutPage /><Footer /></>} />
+            <Route path="/payment" element={<><Navbar /><PaymentPage /><Footer /></>} />
+            <Route path="/payment-success" element={<><Navbar /><FinishPaymentPage /><Footer /></>} />
+            <Route path="/my-orders" element={<><Navbar /><MyOrdersPage /><Footer /></>} />
+            
+            <Route path="/admin" element={<AdminLayout><AdminDashboard /></AdminLayout>} />
+            <Route path="/admin/products" element={<AdminLayout><AdminProducts /></AdminLayout>} />
+            <Route path="/admin/products/new" element={<AdminLayout><AdminAddProduct /></AdminLayout>} />
+            <Route path="/admin/products/edit/:id" element={<AdminLayout><AdminEditProduct /></AdminLayout>} />
+            <Route path="/admin/orders" element={<AdminLayout><AdminOrders /></AdminLayout>} />
+            <Route path="/admin/orders/:id" element={<AdminLayout><AdminOrderDetails /></AdminLayout>} />
+          </Routes>
+        </SmoothScroll>
+      </BrowserRouter>
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <SmoothScroll>
-        <Routes>
-          <Route path="/" element={<><Navbar /><LandingPages /><StoryTelling /><HeroShowcasePage /><Footer /></>} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/Impact" element={<><Navbar/><ImpactPages /><Footer /></>} />
-          <Route path="/Story" element={<><Navbar /><Story /><Footer /></>} />
-          <Route path="/aboutus" element={<><Navbar/><AboutUs /><Footer /></>} />
-          <Route path="/product/:id" element={<><Navbar /><ProductDetailPage /><Footer /></>} />
-          <Route path="/checkout" element={<><Navbar /><CheckoutPage /><Footer /></>} />
-          <Route path="/payment" element={<><Navbar /><PaymentPage /><Footer /></>} />
-          <Route path="/payment-success" element={<><Navbar /><FinishPaymentPage /><Footer /></>} />
-          <Route path="/my-orders" element={<><Navbar /><MyOrdersPage /><Footer /></>} />
-          
-          <Route path="/admin" element={<AdminLayout><AdminDashboard /></AdminLayout>} />
-          <Route path="/admin/products" element={<AdminLayout><AdminProducts /></AdminLayout>} />
-          <Route path="/admin/products/new" element={<AdminLayout><AdminAddProduct /></AdminLayout>} />
-          <Route path="/admin/products/edit/:id" element={<AdminLayout><AdminEditProduct /></AdminLayout>} />
-          <Route path="/admin/orders" element={<AdminLayout><AdminOrders /></AdminLayout>} />
-          <Route path="/admin/orders/:id" element={<AdminLayout><AdminOrderDetails /></AdminLayout>} />
-        </Routes>
-      </SmoothScroll>
-    </BrowserRouter>
+    <RootApp />
   </StrictMode>
 )
+
